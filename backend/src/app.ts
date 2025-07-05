f1380f2dfe3409bc968ebeeec34bf07836e0683f
Gabriell Queiroz <gabriellqs10@gmail.com>
2025-07-05 14:21:03 -0300

    fix: correct CORS configuration between frontend and backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,10 @@ import { verifyTokenForWebSocket } from "./utils/verifyTokenForwebSocket";
 import { router } from "./routes";
 import { config } from "./config";
 import cors from "cors";
-import { initializeUnoGameService, UnoWebSocket } from "./services/unoGameService";
+import {
+  initializeUnoGameService,
+  UnoWebSocket,
+} from "./services/unoGameService";
 import { Console } from "console";
 
 const app = express();
@@ -23,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:8080",
     credentials: true,
   })
 );
@@ -42,7 +45,6 @@ initializeUnoGameService(wss);
 
 // WebSocket authentication function
 
-
 // Handle WebSocket upgrade
 server.on(
   "upgrade",
@@ -54,43 +56,45 @@ server.on(
     const pathname = request.url ? url.parse(request.url).pathname : "";
     const wsPathRegex = /^\/ws\/([a-zA-Z0-9]+)$/;
     const match = pathname?.match(wsPathRegex);
-    console.log(pathname)
+    console.log(pathname);
     if (!match) {
       socket.destroy();
       return;
     }
 
     const roomCode = match[1];
-    console.log(roomCode)
+    console.log(roomCode);
 
     // Parse cookies for authentication
     const cookies = cookie.parse(request.headers.cookie || "");
     const tokenFromCookie = cookies.token;
-    if(tokenFromCookie){
-      const clientData = await verifyTokenForWebSocket(tokenFromCookie)
+    if (tokenFromCookie) {
+      const clientData = await verifyTokenForWebSocket(tokenFromCookie);
 
       if (!clientData) {
-      console.log("WebSocket authentication failed: Invalid or missing token.");
-      socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
-      socket.destroy();
-      return;
-    }
+        console.log(
+          "WebSocket authentication failed: Invalid or missing token."
+        );
+        socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+        socket.destroy();
+        return;
+      }
 
-    wss.handleUpgrade(request, socket, head, (ws) => {
-      // Cast WebSocket to UnoWebSocket and add custom properties
-      const customWs = ws as UnoWebSocket;
-      customWs.playerId = clientData.id as string;
-      customWs.playerName = clientData.name as string;
-      customWs.currentRoomId = roomCode;
+      wss.handleUpgrade(request, socket, head, (ws) => {
+        // Cast WebSocket to UnoWebSocket and add custom properties
+        const customWs = ws as UnoWebSocket;
+        customWs.playerId = clientData.id as string;
+        customWs.playerName = clientData.name as string;
+        customWs.currentRoomId = roomCode;
 
-      console.log("WebSocket handshake successful. Client authenticated.", {
-        playerId: customWs.playerId,
-        playerName: customWs.playerName,
-        roomCode
+        console.log("WebSocket handshake successful. Client authenticated.", {
+          playerId: customWs.playerId,
+          playerName: customWs.playerName,
+          roomCode,
+        });
+
+        wss.emit("connection", customWs, request);
       });
-      
-      wss.emit("connection", customWs, request);
-    });
     }
   }
 );
@@ -99,7 +103,6 @@ server.on(
 server.listen(PORT, () => {
   console.log(`HTTP and WebSocket server running: http://localhost:${PORT}`);
 });
-
 
 app.listen(config.PORT, () =>
   console.log(`Servidor rodando na porta ${config.PORT}`)
