32e144daa95faaa4b8c4bba1c6017e18542d3c77
Gabriell Queiroz <gabriellqs10@gmail.com>
2025-07-05 09:30:42 -0300

    feat: enable CORS for frontend-backend integration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,18 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { router } from "./routes";
 import { config } from "./config";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(router);
 
 app.listen(config.PORT, () =>
