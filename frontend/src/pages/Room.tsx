6a30529d1af8975f35b54290b107f200b573abff
Anderson <andersonpaulojr1@gamil.com>
2025-07-05 10:54:27 -0300

    fix(Room): prevent potential crash when user.id is undefined
    Add null checks for user.id before calling getPlayerOrder to avoid potential runtime errors. Also remove unnecessary dependency from useEffect and add debug console logs.

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -72,8 +72,9 @@ const Room: React.FC = () => {
 
   // Start WebSocket connection when component mounts
   React.useEffect(() => {
+    console.log("teste");
     connect();
-  }, [connect]);
+  }, []);
 
   // Console log user information when component mounts or user changes
   React.useEffect(() => {
@@ -89,11 +90,14 @@ const Room: React.FC = () => {
     if (lastMessage !== null) {
       try {
         const data = JSON.parse(lastMessage.data);
+        console.log(data);
 
         switch (data.type) {
           case "CREATE_ROOM":
             setPlayers(data.room.players);
-            setPlayersOrder(getPlayerOrder(data.room.players, user.id));
+            if (user.id) {
+              setPlayersOrder(getPlayerOrder(data.room.players, user.id));
+            }
             setOwner(true);
             break;
           // case "JOIN_ROOM":
@@ -112,7 +116,9 @@ const Room: React.FC = () => {
             console.log("Room updated:", data);
             setRoomStatus(data.room.status);
             setPlayers(data.room.players);
-            setPlayersOrder(getPlayerOrder(data.room.players, user.id));
+            if (user.id) {
+              setPlayersOrder(getPlayerOrder(data.room.players, user.id));
+            }
             setCurrentPlayerId(data.room.currentPlayerId);
             setCurrentCard(data.room.currentCard);
             setGameDirection(data.room.gameDirection);
